f3792662b7f568052638e89f3f272dd55704b1af
dudakp <palodudak@gmail.com>
2022-08-15 18:27:15 +0200

    feat(data-mgmt): added data-mgmt application

---
+++ data-mgmt/src/test/java/com/pirs/data/datamgmt/DataMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pirs.data.datamgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
