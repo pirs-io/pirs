0e779c2ac353a44edd277d000c568e8f72d82037
dudakp <palodudak@gmail.com>
2022-10-25 00:01:39 +0200

    revert accidental change in tracker module

--- pkg/tracker/domain/register/data.go
+++ pkg/tracker/domain/register/data.go
@@ -7,6 +7,6 @@ import (
 type TrackerInstance struct {
 	redis.ReJsonDocument `json:"-"`
 	OrganizationName     string   `json:"OrganizationName,omitempty"`
-	Url                  string   `json:"RepoRootPath,omitempty"`
+	Url                  string   `json:"Url,omitempty"`
 	RegisteredInstances  []string `json:"RegisteredInstances,omitempty"`
 }
