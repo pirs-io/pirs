6a406dea369815198f64f5cce277a95525c7997e
Matej <matejchvostek@gmail.com>
2022-11-27 10:22:30 +0100

    PIRS-IO-T-12 - implement tests for validation_service

---
+++ pkg/process/mocks/testing_validators.go
@@ -0,0 +1,62 @@
+package mocks
+
+import (
+	"pirs.io/process/validation/models"
+)
+
+// ImportProcessRequestValidator request
+type ImportProcessRequestValidator struct {
+	MockResult bool
+	next       models.Validator
+}
+
+func (rv *ImportProcessRequestValidator) Validate(data *models.ImportProcessValidationData) {
+	data.ValidationFlags.IsRequestValid = rv.MockResult
+	rv.ExecuteNextIfPresent(data)
+}
+func (rv *ImportProcessRequestValidator) ExecuteNextIfPresent(data *models.ImportProcessValidationData) {
+	if rv.next != nil {
+		rv.next.Validate(data)
+	}
+}
+func (rv *ImportProcessRequestValidator) SetNext(validator models.Validator) {
+	rv.next = validator
+}
+
+// FileTypeValidator file type
+type FileTypeValidator struct {
+	MockResult bool
+	next       models.Validator
+}
+
+func (ft *FileTypeValidator) Validate(data *models.ImportProcessValidationData) {
+	data.ValidationFlags.IsFileTypeValid = ft.MockResult
+	ft.ExecuteNextIfPresent(data)
+}
+func (ft *FileTypeValidator) ExecuteNextIfPresent(data *models.ImportProcessValidationData) {
+	if ft.next != nil {
+		ft.next.Validate(data)
+	}
+}
+func (ft *FileTypeValidator) SetNext(validator models.Validator) {
+	ft.next = validator
+}
+
+// SchemaValidator schema
+type SchemaValidator struct {
+	MockResult bool
+	next       models.Validator
+}
+
+func (sv *SchemaValidator) Validate(data *models.ImportProcessValidationData) {
+	data.ValidationFlags.IsSchemaValid = sv.MockResult
+	sv.ExecuteNextIfPresent(data)
+}
+func (sv *SchemaValidator) ExecuteNextIfPresent(data *models.ImportProcessValidationData) {
+	if sv.next != nil {
+		sv.next.Validate(data)
+	}
+}
+func (sv *SchemaValidator) SetNext(validator models.Validator) {
+	sv.next = validator
+}
--- pkg/process/validation/service/validation_service_test.go
+++ pkg/process/validation/service/validation_service_test.go
@@ -1,1 +1,50 @@
 package service
+
+import (
+	"github.com/stretchr/testify/assert"
+	"pirs.io/process/mocks"
+	"pirs.io/process/service/models"
+	valModels "pirs.io/process/validation/models"
+	"testing"
+)
+
+func TestValidationService_ValidateProcessData(t *testing.T) {
+	valData := &valModels.ImportProcessValidationData{
+		ReqData:         models.ImportProcessRequestData{},
+		ValidationFlags: valModels.ValidationFlags{},
+	}
+
+	// is valid
+	chainSucess := buildValidationChainsTest(false)
+	vsSucess := &ValidationService{
+		chainStart: chainSucess,
+	}
+	result := vsSucess.ValidateProcessData(valData)
+	assert.Equal(t, true, result)
+
+	// is invalid
+	chainFail := buildValidationChainsTest(true)
+	vsFail := &ValidationService{
+		chainStart: chainFail,
+	}
+	result = vsFail.ValidateProcessData(valData)
+	assert.Equal(t, false, result)
+}
+
+func buildValidationChainsTest(isFail bool) valModels.Validator {
+	// define validators
+	requestValidator := &mocks.ImportProcessRequestValidator{
+		MockResult: !isFail,
+	}
+	fileTypeValidator := &mocks.FileTypeValidator{
+		MockResult: true,
+	}
+	schemaValidator := &mocks.SchemaValidator{
+		MockResult: true,
+	}
+	// create chain
+	requestValidator.SetNext(fileTypeValidator)
+	fileTypeValidator.SetNext(schemaValidator)
+
+	return requestValidator
+}
