3a10d4a88547f2559bf39377058998cd783a08bf
Matej <matejchvostek@gmail.com>
2023-02-18 09:33:03 +0100

    PIRS-IO-T-30 - add docstrings

--- pkg/dependency-management/service/models/detect_model.go
+++ pkg/dependency-management/service/models/detect_model.go
@@ -6,13 +6,15 @@ import (
 	"pirs.io/process/domain"
 )
 
-// A DetectRequestData todo
+// A DetectRequestData represents input inside service.DetectionService. The instance is created in GRPC server. A CheckSum
+// is checksum of ProcessData.
 type DetectRequestData struct {
 	CheckSum    string
 	ProcessData bytes.Buffer
 }
 
-// A DetectResponseData todo
+// A DetectResponseData represents output for GRPC server from service.DetectionService. Metadata is array of metadata,
+// that are dependent on input process data.
 type DetectResponseData struct {
 	Status   codes.Code
 	Metadata []domain.Metadata
